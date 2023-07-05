bf8e75b529ee184b0c3a22854d2e282ba5b73fa5
Liudimilla Silva <42663858+Liudimilla@users.noreply.github.com>
2023-07-05 11:07:56 -0300

    Update InclusaoDeDados.sql
    Atualizaçao de dados

--- Empresa/InclusaoDeDados.sql
+++ Empresa/InclusaoDeDados.sql
@@ -13,12 +13,12 @@ INSERT INTO PRODUTOS (
 INSERT INTO PRODUTOS (
     CODIGO, DESCRITOR, SABOR, TAMANHO, EMBALAGEM, PRECO_LISTA
 ) VALUES (
-    '1040109', 'Light - 350 ml - AÁai' , 'AÁai', '350 ml', 'Lata', 5.60
+    '1040109', 'Light - 350 ml - A√ßai' , 'A√ßai', '350 ml', 'Lata', 5.60
 );
 INSERT INTO PRODUTOS 
 VALUES 
 ('1040115', 'Light - 350 ml ' , 'Manga', '350 ml', 'Lata', 3.20),
-('1040116', 'Light - 350 ml ' , 'MaÁa', '350 ml', 'Lata', 4.20),
+('1040116', 'Light - 350 ml ' , 'Ma√ßa', '350 ml', 'Lata', 4.20),
 ('1040117', 'Light - 350 ml ' , 'Laranja', '350 ml', 'Lata', 3.19),
 ('1040118', 'Light - 350 ml ', 'Pessego', '350 ml', 'Lata', 4.25);
 
@@ -26,11 +26,11 @@ INSERT INTO [dbo].[CLIENTES] ([CPF], [NOME], [ENREDECO],
     [BAIRRO], [CIDADE], [ESTADO], [CEP], [DATA_NASCIMENTO], 
     [IDADE], [GENERO], [LIMITE_CREDITO] ,[VOLUME_COMPRA], 
     [PRIMAIRA_COMPRA])
-VALUES ('1471156710', '…rica Carvalho', 'R. Iriquitia', 'Jardins', 'S„o Paulo', 
+VALUES ('1471156710', '√ârica Carvalho', 'R. Iriquitia', 'Jardins', 'S√£o Paulo', 
         'SP', '80012212', '19900901', 27, 'F', 170000, 24500,0), 
-    ('19290992743', 'Fernando Cavalcante', 'R. Dois de Fevereiro', '¡gua Santa', 
+    ('19290992743', 'Fernando Cavalcante', 'R. Dois de Fevereiro', '√Ågua Santa', 
         'Rio de Janeiro', 'RJ', '22000000', '20000212', 18, 'M', 100000, 20000, 1), 
-    ('2600586709', 'CÈsar Teixeira', 'Rua Conde de Bonfim', 'Tijuca', 
+    ('2600586709', 'C√©sar Teixeira', 'Rua Conde de Bonfim', 'Tijuca', 
         'Rio de Janeiro', 'RJ', '22020001', '20000312', 18, 'M', 120000, 22000, 0);
 
 ----
@@ -46,10 +46,25 @@ INSERT INTO [dbo].[PRODUTOS]
            ,[PRECO_LISTA])
      VALUES
            ('1040119'
-            ,'Light - 350 ml - Lim„o'
-            ,'Lim„o'
+            ,'Light - 350 ml - Lim√£o'
+            ,'Lim√£o'
             ,'350 ml'
             ,'Lata'
             ,3.25)
 GO
 -------
+INSERT INTO PRODUTOS 
+    (CODIGO,DESCRITOR,SABOR,TAMANHO,EMBALAGEM,PRECO_LISTA)
+VALUES 
+    ('1001001','Sabor dos Alpes 700 ml - Manga','Manga','700 ml','Garrafa',7.50),
+    ('1001000','Sabor dos Alpes 700 ml - Mel√£o','Mel√£o','700 ml','Garrafa',7.50),
+    ('1001002','Sabor dos Alpes 700 ml - Graviola','Graviola','700 ml','Garrafa',7.50),
+    ('1001003','Sabor dos Alpes 700 ml - Tangerina','Tangerina','700 ml','Garrafa',7.50),
+    ('1001004','Sabor dos Alpes 700 ml - Abacate','Abacate','700 ml','Garrafa',7.50),
+    ('1001005','Sabor dos Alpes 700 ml - A√ßai','A√ßai','700 ml','Garrafa',7.50),
+    ('1001006','Sabor dos Alpes 1 Litro - Manga','Manga','1 Litro','Garrafa',7.50),
+    ('1001007','Sabor dos Alpes 1 Litro - Mel√£o','Mel√£o','1 Litro','Garrafa',7.50),
+    ('1001008','Sabor dos Alpes 1 Litro - Graviola','Graviola','1 Litro','Garrafa',7.50),
+    ('1001009','Sabor dos Alpes 1 Litro - Tangerina','Tangerina','1 Litro','Garrafa',7.50),
+    ('1001010','Sabor dos Alpes 1 Litro - Abacate','Abacate','1 Litro','Garrafa',7.50),
+    ('1001011','Sabor dos Alpes 1 Litro - A√ßai','A√ßai','1 Litro','Garrafa',7.50);
